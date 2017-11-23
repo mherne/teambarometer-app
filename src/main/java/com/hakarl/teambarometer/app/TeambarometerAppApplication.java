b21823933a5d52b006cbf973f0442dfb3ad92338
Armann David Sigurdsson <armannds@gmail.com>
2017-11-23 15:56:48 +0100

    Add project skeleton

---
+++ src/main/java/com/hakarl/teambarometer/app/TeambarometerAppApplication.java
@@ -0,0 +1,12 @@
+package com.hakarl.teambarometer.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeambarometerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeambarometerAppApplication.class, args);
+	}
+}
