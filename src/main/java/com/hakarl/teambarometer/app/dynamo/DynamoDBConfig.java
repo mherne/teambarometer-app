675e50e8e7209a777a00aeb2f04ddb38b145ccee
Armann David Sigurdsson <armannds@gmail.com>
2017-11-24 14:35:29 +0100

    Add jar file configuration

---
+++ src/main/java/com/hakarl/teambarometer/app/dynamo/DynamoDBConfig.java
@@ -0,0 +1,27 @@
+package com.hakarl.teambarometer.app.config;
+
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author armannds
+ * @since 2017-11-24
+ */
+@EnableDynamoDBRepositories(basePackages = "com.hakarl.teambarometer.app")
+@Configuration
+public class DynamoDBConfig {
+
+    @Bean
+    public AmazonDynamoDB amazonDynamoDB() {
+        return AmazonDynamoDBClientBuilder.defaultClient();
+    }
+
+    @Bean
+    public DynamoDBMapper dynamoDBMapper() {
+        return new DynamoDBMapper(amazonDynamoDB());
+    }
+}
---
+++ src/main/java/com/hakarl/teambarometer/app/models/Question.java
@@ -0,0 +1,8 @@
+package com.hakarl.teambarometer.app.models;
+
+/**
+ * @author armannds
+ * @since 2017-11-24
+ */
+public class Question {
+}
